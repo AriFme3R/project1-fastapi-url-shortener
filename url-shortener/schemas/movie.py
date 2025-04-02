d098616838580b06aa0351c1cf51631184d6ded7
Nikolay Ivashchenko <programswork2@gmail.com>
2025-04-03 07:52:46 +0900

    create MovieCreate schema

--- url-shortener/schemas/movie.py
+++ url-shortener/schemas/movie.py
@@ -1,9 +1,24 @@
+from typing import Annotated
+
+from annotated_types import Len
+
 from pydantic import BaseModel
 
 
 class MovieBase(BaseModel):
     id: int
     title: str
+    description: str
+    year: int
+    duration: int
+
+
+class MovieCreate(MovieBase):
+    """
+    Модель для создания фильма
+    """
+
+    title: Annotated[str, Len(min_length=8, max_length=50)]
     description: str
     year: int
     duration: int
