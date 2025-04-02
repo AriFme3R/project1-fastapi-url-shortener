33ac6a7ba66c6e0fa0b08a412fbe4b026e42fe32
Nikolay Ivashchenko <programswork2@gmail.com>
2025-04-03 06:42:52 +0900

    refactoring: API router for main

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -7,16 +7,18 @@ from fastapi import (
     status,
     Depends,
 )
-from fastapi.responses import (
-    RedirectResponse,
-)
 
-from schemas.shortened_url import ShortenedUrl
+from api import router as api_router
+from api.redirect_views import router as redirect_views
+
 from schemas.movie import Movie
 
 app = FastAPI(
     title="URL Shortener",
 )
+
+app.include_router(redirect_views)
+app.include_router(api_router)
 
 
 @app.get("/")
@@ -32,67 +34,6 @@ def read_root(
         "Message": f"Hello, {name}!",
         "docs": str(docs_url),
     }
-
-
-SHORTENED_URLS = [
-    ShortenedUrl(
-        target_url="https://example.com",
-        slug="example",
-    ),
-    ShortenedUrl(
-        target_url="https://google.com",
-        slug="search",
-    ),
-]
-
-
-@app.get(
-    "/short_urls/",
-    response_model=list[ShortenedUrl],
-)
-def read_short_urls_list():
-    return SHORTENED_URLS
-
-
-def prefetch_shortened_url(
-    slug: str,
-) -> ShortenedUrl:
-    url: ShortenedUrl | None = next(
-        (url for url in SHORTENED_URLS if url.slug == slug),
-        None,
-    )
-    if url:
-        return url
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"URL {slug!r} not found",
-    )
-
-
-@app.get("/r/{slug}")
-@app.get("/r/{slug}/")
-def redirect_shortened_url(
-    url: Annotated[
-        ShortenedUrl,
-        Depends(prefetch_shortened_url),
-    ],
-):
-    return RedirectResponse(
-        url=url.target_url,
-    )
-
-
-@app.get(
-    "/short-urls/{slug}/",
-    response_model=ShortenedUrl,
-)
-def read_shortened_url_details(
-    url: Annotated[
-        ShortenedUrl,
-        Depends(prefetch_shortened_url),
-    ],
-) -> ShortenedUrl:
-    return url
 
 
 MOVIES = [
@@ -144,7 +85,7 @@ def prefetch_movie(
 
 
 @app.get(
-    "/movie/{movie_id}/",
+    "/movies/{movie_id}/",
     response_model=Movie,
 )
 def read_movie_details(
