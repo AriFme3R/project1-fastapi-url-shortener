0f33c2d74f57e76744c1ed3ad10f6cf475258f36
Nikolay Ivashchenko <programswork2@gmail.com>
2025-04-03 07:45:05 +0900

    create ShortenedUrlCreate schema

--- url-shortener/schemas/shortened_url.py
+++ url-shortener/schemas/shortened_url.py
@@ -1,9 +1,23 @@
+from typing import Annotated
+
 from pydantic import BaseModel, AnyHttpUrl
+from annotated_types import Len
 
 
 class ShortenedUrlBase(BaseModel):
     target_url: AnyHttpUrl
     slug: str
+
+
+class ShortenedUrlCreate(ShortenedUrlBase):
+    """
+    Модель для создания сокращённой ссылки
+    """
+
+    slug: Annotated[
+        str,
+        Len(min_length=3, max_length=10),
+    ]
 
 
 class ShortenedUrl(ShortenedUrlBase):
