b0e566c5e15083c3223cf5c37fd233a820bb32ed
Nikolay Ivashchenko <programswork2@gmail.com>
2025-07-31 16:04:30 +0900

    Add method for check url from ShortenedUrlCreateSchema

--- url-shortener/testing/test_schemas/test_shortened_url.py
+++ url-shortener/testing/test_schemas/test_shortened_url.py
@@ -20,3 +20,27 @@ class ShortenedUrlCreateTestCase(TestCase):
         self.assertEqual(shortened_url_in.slug, shortened_url.slug)
         self.assertEqual(shortened_url_in.target_url, shortened_url.target_url)
         self.assertEqual(shortened_url_in.description, shortened_url.description)
+
+    def test_shortened_url_create_accepts_different_urls(self) -> None:
+        urls = [
+            "http://example.com",
+            "https://example.com",
+            # "rtmp://video.example.com",
+            # "rtmps://video.example.com",
+            "http://abc.example.com",
+            "https://www.example.com/foobar/",
+        ]
+        for url in urls:
+            with self.subTest(url=url, msg=f"test-url-{url}"):
+                shortened_url_create = ShortenedUrlCreate(
+                    slug="some-slug",
+                    description="some-description",
+                    target_url=url,
+                )
+
+                self.assertEqual(
+                    url.rstrip("/"),
+                    shortened_url_create.model_dump(mode="json")["target_url"].rstrip(
+                        "/",
+                    ),
+                )
